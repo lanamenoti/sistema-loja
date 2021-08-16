4bd8d64b01cb1edbec83b49fdce12e1a7bdcb95c
lanamenoti <alana.pires@acad.pucrs.br>
2021-08-16 17:53:47 -0300

    adicionando inicio do projeto

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          Logo
+        </div>
+        <div class="col-md-6 header__right">
+          menu
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "Header",
+}
+</script>
+<style>
+.header {
+  background-color: rgb(91, 247, 195);
+  height: 80px;
+  width: 100%;
+}
+
+.header__right {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <li>Clientes</li>
+      <li>Pedidos</li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Menu",
+}
+</script>
+<style></style>
